5f76a87c79e124b2303ed13c209d1e0e46c693fc
ivan17choperz <ivan17choperz@gmail.com>
2023-11-20 18:35:14 -0500

    finished details

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  constructor(private router: Router) {}
 
+  currenUser = JSON.parse(localStorage.getItem('blossom_user')!);
+
+  logOut() {
+    localStorage.clear();
+    this.router.navigateByUrl('/auth/login');
+  }
 }
