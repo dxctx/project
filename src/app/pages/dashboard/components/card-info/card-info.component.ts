a0de4ebeaeae0bba859dcd6f158f1c0b46e5c2e5
ivan17choperz <ivan17choperz@gmail.com>
2023-11-19 23:21:50 -0500

    send data a card-info component

--- src/app/pages/dashboard/components/card-info/card-info.component.ts
+++ src/app/pages/dashboard/components/card-info/card-info.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PrimengModule } from 'src/app/primeng.module';
-import { StaffService } from 'src/app/services/staff.service';
+import { Person, StaffService } from 'src/app/services/staff.service';
 
 @Component({
   selector: 'app-card-info',
   templateUrl: 'card-info.component.html',
 })
 export class CardInfoComponent implements OnInit {
-  constructor(private staffServices:StaffService) {}
+  @Input() persons: Person[] = [];
+  constructor() {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 }
